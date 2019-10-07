1510473a4d14bb1a70010823bec738803ec68af7
Nurul Hakim <nurulhakim2810@gmail.com>
2019-10-07 13:15:09 +0700

    Create latihan2.php

---
+++ minggu6/latihan2.php
@@ -0,0 +1,34 @@
+<?php
+//KALKULATOR LATIHAN 2
+$a = 15;
+$a = 5;
+
+echo "bilangan 1 = $a"; echo "<br>";
+echo "bilangan 2 = $b"; echo "<br>";
+echo "Berikut hasil perhitungan dari setiap operator"; echo "<br";
+
+    echo "<br/> PENJUMLAHAN <br/>";
+    $c = $a+$b;
+    echo "operator =+"; echo "<br>";
+    echo "hasilnya : $a + $b = $c";
+    echo "<br/>";
+    echo "<br/> PENGURANGAN <br/>";
+    $d = $a-$b;
+    echo "operator =-"; echo "<br>";
+    echo "hasilnya : $a - $b = $d";
+    echo "<br/>";
+    echo "<br/> PERKALIAN <br/>";
+    $e = $a*$b;
+    echo "operator =*"; echo "<br>";
+    echo "hasilnya : $a * $b = $e";
+    echo "<br/>";
+    echo "<br/> PEMBAGIAN <br/>";
+    $f = $a/$b;
+    echo "operator =/"; echo "<br>";
+    echo "hasilnya : $a / $b = $f";
+    echo "<br/>";
+    echo "<br/> MODULUS <br/>";
+    $f = $a%$b;
+    echo "operator =%"; echo "<br>";
+    echo "hasilnya : $a % $b = $g";
+    echo "<br/>";
