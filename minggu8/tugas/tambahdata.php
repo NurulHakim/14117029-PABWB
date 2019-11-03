36825c2fd97383dd24fc7a2eabe516585ed95032
Nurul Hakim <nurulhakim2810@gmail.com>
2019-11-03 14:30:07 +0700

    Update tambahdata.php

--- minggu8/tugas/tambahdata.php
+++ minggu8/tugas/tambahdata.php
@@ -7,17 +7,33 @@
     <?php
     $nrp = $_POST["nrp"];
     $nama = $_POST["nama"];
-    $foto = $_POST["foto"];
     $jurusan = $_POST["jurusan"];
-    $conn = mysqli_connect ("localhost","root","") or die ("Koneksi tidak berhasil");
+    if($jurusan == "Telekomunikasi"){
+        $id_jurusan = "1";
+    }
+    else if($jurusan == "Elka"){
+        $id_jurusan = "2";
+    }
+    else if ($jurusan == "IT"){
+        $id_jurusan = "3";
+    }
+    else{
+        $id_jurusan = "4";
+    }
+    $target_dir = "gambar/";
+    $target_file = $target_dir . basename($_FILES["foto"]["name"]);
+    $files = $_FILES["foto"]["tmp_name"];
+    move_uploaded_file(string '$files', string '$target_file');
+            
 
+    $conn = mysqli_connect ("localhost","root","") or die ("Koneksi tidak berhasil");
+    
     mysqli_select_db($conn, "kampus");
 
     $sqlstr =  "insert into mahasiswa (NRP, Nama, Foto, ID_Jur)
-                values ('$nrp','$nama','$foto','$jurusan')";
-    $hasil = mysqli_query($sqlstr, $conn);
+                values ('$nrp','$nama','$target_file','$id_jurusan')";
+    $hasil = mysqli_query($conn, $sqlstr);
     echo "Tambah data berhasil!";
 
     ?>
-    </body>
-    </html>
+    
