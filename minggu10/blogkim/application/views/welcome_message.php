49590dcdda51745b276661340417eaaa1ebc959a
Nurul Hakim <nurulhakim2810@gmail.com>
2019-11-19 00:40:27 +0700

    Minggu 10

---
+++ minggu10/blogkim/application/views/welcome_message.php
@@ -0,0 +1,243 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-gb" class="no-js">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!--[if lt IE 9]> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <![endif]-->
+    <title>Personal Website - Nurul Hakim</title>
+    <meta name="description" content="">
+    <meta name="author" content="WebThemez">
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lte IE 8]>
+		<script type="text/javascript" src="http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
+	<![endif]-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/isotope.css" media="screen" />
+    <link rel="stylesheet" href="assets/js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="assets/css/da-slider.css" />
+    <!-- Owl Carousel Assets -->
+    <link href="assets/js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css" />
+    <!-- Font Awesome -->
+    <link href="assets/font/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand scroll-top logo"><b>Personal Website</b></a>
+                </div>
+                <!--/.navbar-header-->
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav" id="mainNav">
+                        <li class="active"><a href="#home" class="scroll-link">Home</a></li>
+                        <li><a href="#aboutUs" class="scroll-link">About Us</a></li>
+                        <li><a href="#contactUs" class="scroll-link">Contact Us</a></li>
+                    </ul>
+                </div>
+                <!--/.navbar-collapse-->
+            </nav>
+            <!--/.navbar-->
+        </div>
+        <!--/.container-->
+    </header>
+    <!--/.header-->
+    <div id="#top"></div>
+    <section id="home">
+        <div class="banner-container">
+            <img src="assets/images/banner-bg.jpg" alt="banner" />
+            <div class="container banner-content">
+                <div id="da-slider" class="da-slider">
+                    <div class="da-slide">
+                        <h2>Informatics Engineering Student</h2>
+                        <p>Studied at the Sumatran Technological Institute</p>
+                        <div class="da-img"></div>
+                    </div>
+                    <div class="da-slide">
+                        <h2>KSR PMI Unit ITERA</h2>
+                        <p>Volunter of Palang Merah Indonesia</p>
+                        <div class="da-img"></div>
+                    </div>
+                    <div class="da-slide">
+                        <h2>HTML5/CSS</h2>
+                        <p>HTML5 is a markup language used for structuring and presenting Web.</p>
+                        <div class="da-img"></div>
+                    </div>
+                    
+				<!--  <nav class="da-arrows">
+                        <span class="da-arrows-prev"></span>
+                        <span class="da-arrows-next"></span>
+                    </nav> -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="introText">
+        <div class="container">
+            <div class="text-center">
+            <h1>Selamat datang di Personal Website Nurul Hakim</h1>
+              <p>Seorang mahasiswa yang Ngambis tiap waktu! Semangat gaess ;)</p>
+            </div>
+        </div>
+
+    </section>
+    <!--About-->
+    <section id="aboutUs" class="secPad">
+        <div class="container">
+            <div class="heading text-center">
+                <!-- Heading -->
+                <h2>Nurul Hakim</h2>
+                <p>14117029</p>
+            </div>
+            <div class="row">
+                <!-- item -->
+                <div class="col-md-4 text-center tileBox">
+                   <div class="txtHead"> <i class="fa fa-desktop"></i>
+                    <h3>Informatics Engineering <span class="id-color">Student</span></h3></div>
+                    <p>Sedang menempuh studi Strata-1 Teknik Informatika di Institut Teknologi Sumatera</p>
+                </div>
+                <!-- end: -->
+
+                <!-- item -->
+                <div class="col-md-4 text-center tileBox">
+                    <div class="txtHead"><i class="fa fa-css3"></i>
+                    <h3>Are Studying <span class="id-color">HTML 5 and CSS</span></h3></div>
+                    <p>SEMANGATTTT!</p>
+                </div>
+                <!-- end: -->
+
+                <!-- item -->
+                <div class="col-md-4 text-center tileBox">
+                    <div class="txtHead"><i class="fa fa-lightbulb-o"></i>
+                    <h3>Kors Sukarela PMI<span class="id-color"> Unit ITERA</span></h3></div>
+                    <p>Korps Sukarela (KSR) adalah kesatuan unit PMI yang menjadi wadah bagi anggota biasa dan perseorangan yang atas kesadaran sendiri menyatakan menjadi anggota KSR.</p>
+                </div>
+                <!-- end: -->
+            </div>
+        </div>
+    </section>
+    <!--Quote-->
+    <section id="quote" class="bg-parlex">
+        <div class="parlex-back">
+            <div class="container secPad text-center">
+               <h2>Life is a game.</h2><p><b>(QS. Al-An'am : 32)</b></p>
+            </div>
+            <!--/.container-->
+        </div>
+    </section>
+    
+     
+  
+   
+	<!--Contact -->
+    <section id="contactUs" class="page-section secPad">
+        <div class="container">
+
+            <div class="row">
+                <div class="heading text-center">
+                    <!-- Heading -->
+                    <h2>Let's Keep In Touch!</h2>
+                    <p>Thank you for visiting out my profile. If you would like to get into contact with me, please fill out the form below.</p>
+                </div>
+            </div>
+
+            <div class="row mrgn30">
+
+                <form method="post" action="" id="contactfrm" role="form">
+
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your name (at least 2 characters)">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email" title="Please enter a valid email address">
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="comments">Comments</label>
+                            <textarea name="comment" class="form-control" id="comments" cols="3" rows="5" placeholder="Enter your message…" title="Please enter your message (at least 10 characters)"></textarea>
+                        </div>
+                        <button name="submit" type="submit" class="btn btn-lg btn-primary" id="submit">Submit</button>
+                        <div class="result"></div>
+                    </div>
+                </form>
+                <div class="col-sm-4">
+                    <h4>Address:</h4>
+                    <address>
+                        Jl. Sultan Jamil No. 29<br>
+                        Kel Gedong Meneng, Kec Rajabasa<br>
+                        Bandar Lampung, Lampung
+                        <br>
+                    </address>
+                    <h4>Phone:</h4>
+                    <address>
+                        +6282281490501<br>
+                    </address>
+                </div>
+            </div>
+        </div>
+        <!--/.container-->
+    </section>
+    <footer>
+        <div class="container">
+            <div class="social text-center">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-flickr"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+            </div>
+
+            <div class="clear"></div>
+            <!--CLEAR FLOATS-->
+        </div>
+    </footer>
+    <!--/.page-section-->
+    <section class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    Copyright 2014 | All Rights Reserved  -- Template by <a href="http://webThemez.com">WebThemez.com</a>
+                </div>
+            </div>
+            <!-- / .row -->
+        </div>
+    </section>
+    <a href="#top" class="topHome"><i class="fa fa-chevron-up fa-2x"></i></a>
+
+    <!--[if lte IE 8]><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><![endif]-->
+    <script src="assets/js/modernizr-latest.js"></script>
+    <script src="assets/js/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.isotope.min.js" type="text/javascript"></script>
+    <script src="assets/js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.nav.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.cslider.js" type="text/javascript"></script>
+    <script src="assets/js/custom.js" type="text/javascript"></script>
+    <script src="assets/js/owl-carousel/owl.carousel.js"></script>
+</body>
+</html>
+?>
